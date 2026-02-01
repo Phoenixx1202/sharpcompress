ddd42dc5c55c12e8708037990be21cb01ac181b7
Maurício <mcnet.fenix@gmail.com>
2026-02-01 00:47:07 -0400

    Refactor Utility class and improve methods

--- src/SharpCompress/Utility.cs
+++ src/SharpCompress/Utility.cs
@@ -1,165 +1,137 @@
 using System;
-using System.Buffers;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.IO;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
+using System.Linq;
 
-namespace SharpCompress;
-
-internal static partial class Utility
+namespace SharpCompress
 {
-    //80kb is a good industry standard temporary buffer size
-    internal const int TEMP_BUFFER_SIZE = 81920;
-    private static readonly HashSet<char> invalidChars = new(Path.GetInvalidFileNameChars());
-
-    public static ReadOnlyCollection<T> ToReadOnly<T>(this IList<T> items) => new(items);
-
-    /// <summary>
-    /// Performs an unsigned bitwise right shift with the specified number
-    /// </summary>
-    /// <param name="number">Number to operate on</param>
-    /// <param name="bits">Amount of bits to shift</param>
-    /// <returns>The resulting number from the shift operation</returns>
-    public static int URShift(int number, int bits) => (int)((uint)number >> bits);
-
-    /// <summary>
-    /// Performs an unsigned bitwise right shift with the specified number
-    /// </summary>
-    /// <param name="number">Number to operate on</param>
-    /// <param name="bits">Amount of bits to shift</param>
-    /// <returns>The resulting number from the shift operation</returns>
-    public static long URShift(long number, int bits) => (long)((ulong)number >> bits);
-
-    public static void SetSize(this List<byte> list, int count)
+    internal static partial class Utility
     {
-        if (count > list.Count)
+        // 80kb is a good industry standard temporary buffer size
+        internal const int TEMP_BUFFER_SIZE = 81920;
+        private static readonly HashSet<char> invalidChars = new HashSet<char>(Path.GetInvalidFileNameChars());
+
+        public static ReadOnlyCollection<T> ToReadOnly<T>(this IList<T> items) => new ReadOnlyCollection<T>(items);
+
+        public static int URShift(int number, int bits) => (int)((uint)number >> bits);
+
+        public static long URShift(long number, int bits) => (long)((ulong)number >> bits);
+
+        public static void SetSize(this List<byte> list, int count)
         {
-            // Ensure the list only needs to grow once
-            list.Capacity = count;
-            for (var i = list.Count; i < count; i++)
+            if (count > list.Count)
             {
-                list.Add(0x0);
+                list.Capacity = count;
+                for (var i = list.Count; i < count; i++)
+                {
+                    list.Add(0x0);
+                }
+            }
+            else
+            {
+                list.RemoveRange(count, list.Count - count);
             }
         }
-        else
+
+        public static void ForEach<T>(this IEnumerable<T> items, Action<T> action)
         {
-            list.RemoveRange(count, list.Count - count);
-        }
-    }
-
-    public static void ForEach<T>(this IEnumerable<T> items, Action<T> action)
-    {
-        foreach (var item in items)
-        {
-            action(item);
-        }
-    }
-
-    public static IEnumerable<T> AsEnumerable<T>(this T item)
-    {
-        yield return item;
-    }
-
-    public static DateTime DosDateToDateTime(ushort iDate, ushort iTime)
-    {
-        var year = (iDate / 512) + 1980;
-        var month = iDate % 512 / 32;
-        var day = iDate % 512 % 32;
-        var hour = iTime / 2048;
-        var minute = iTime % 2048 / 32;
-        var second = iTime % 2048 % 32 * 2;
-
-        if (iDate == ushort.MaxValue || month == 0 || day == 0)
-        {
-            year = 1980;
-            month = 1;
-            day = 1;
+            foreach (var item in items)
+            {
+                action(item);
+            }
         }
 
-        if (iTime == ushort.MaxValue)
+        public static IEnumerable<T> AsEnumerable<T>(this T item)
         {
-            hour = minute = second = 0;
+            yield return item;
         }
 
-        DateTime dt;
-        try
+        public static DateTime DosDateToDateTime(ushort iDate, ushort iTime)
         {
-            dt = new DateTime(year, month, day, hour, minute, second, DateTimeKind.Local);
-        }
-        catch
-        {
-            dt = new DateTime();
-        }
-        return dt;
-    }
+            var year = (iDate / 512) + 1980;
+            var month = iDate % 512 / 32;
+            var day = iDate % 512 % 32;
+            var hour = iTime / 2048;
+            var minute = iTime % 2048 / 32;
+            var second = iTime % 2048 % 32 * 2;
 
-    public static uint DateTimeToDosTime(this DateTime? dateTime)
-    {
-        if (dateTime is null)
-        {
-            return 0;
-        }
+            if (iDate == ushort.MaxValue || month == 0 || day == 0)
+            {
+                year = 1980;
+                month = 1;
+                day = 1;
+            }
 
-        var localDateTime = dateTime.Value.ToLocalTime();
+            if (iTime == ushort.MaxValue)
+            {
+                hour = minute = second = 0;
+            }
 
-        return (uint)(
-            (localDateTime.Second / 2)
-            | (localDateTime.Minute << 5)
-            | (localDateTime.Hour << 11)
-            | (localDateTime.Day << 16)
-            | (localDateTime.Month << 21)
-            | ((localDateTime.Year - 1980) << 25)
-        );
-    }
-
-    public static DateTime DosDateToDateTime(uint iTime) =>
-        DosDateToDateTime((ushort)(iTime / 65536), (ushort)(iTime % 65536));
-
-    /// <summary>
-    /// Convert Unix time value to a DateTime object.
-    /// </summary>
-    /// <param name="unixtime">The Unix time stamp you want to convert to DateTime.</param>
-    /// <returns>Returns a DateTime object that represents value of the Unix time.</returns>
-    public static DateTime UnixTimeToDateTime(long unixtime)
-    {
-        var sTime = new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc);
-        return sTime.AddSeconds(unixtime);
-    }
-
-    extension(Stream source)
-    {
-        public long TransferTo(Stream destination, long maxLength)
-        {
-            // Use ReadOnlySubStream to limit reading and leverage framework's CopyTo
-            using var limitedStream = new IO.ReadOnlySubStream(source, maxLength);
-            limitedStream.CopyTo(destination, TEMP_BUFFER_SIZE);
-            return limitedStream.Position;
+            DateTime dt;
+            try
+            {
+                dt = new DateTime(year, month, day, hour, minute, second, DateTimeKind.Local);
+            }
+            catch
+            {
+                dt = new DateTime();
+            }
+            return dt;
         }
 
-        public async ValueTask<long> TransferToAsync(
-            Stream destination,
-            long maxLength,
-            CancellationToken cancellationToken = default
-        )
+        public static uint DateTimeToDosTime(this DateTime? dateTime)
         {
-            // Use ReadOnlySubStream to limit reading and leverage framework's CopyToAsync
-            using var limitedStream = new IO.ReadOnlySubStream(source, maxLength);
-            await limitedStream
-                .CopyToAsync(destination, TEMP_BUFFER_SIZE, cancellationToken)
-                .ConfigureAwait(false);
-            return limitedStream.Position;
-        }
-    }
+            if (dateTime == null)
+            {
+                return 0;
+            }
 
-    extension(Stream source)
-    {
-        public async ValueTask SkipAsync(
-            long advanceAmount,
-            CancellationToken cancellationToken = default
-        )
+            var localDateTime = dateTime.Value.ToLocalTime();
+
+            return (uint)(
+                (localDateTime.Second / 2)
+                | (localDateTime.Minute << 5)
+                | (localDateTime.Hour << 11)
+                | (localDateTime.Day << 16)
+                | (localDateTime.Month << 21)
+                | ((localDateTime.Year - 1980) << 25)
+            );
+        }
+
+        public static DateTime DosDateToDateTime(uint iTime) =>
+            DosDateToDateTime((ushort)(iTime / 65536), (ushort)(iTime % 65536));
+
+        public static DateTime UnixTimeToDateTime(long unixtime)
+        {
+            var sTime = new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc);
+            return sTime.AddSeconds(unixtime);
+        }
+
+
+        public static long TransferTo(this Stream source, Stream destination, long maxLength)
+        {
+
+            using (var limitedStream = new SharpCompress.IO.ReadOnlySubStream(source, maxLength))
+            {
+                limitedStream.CopyTo(destination, TEMP_BUFFER_SIZE);
+                return limitedStream.Position;
+            }
+        }
+
+        public static async Task<long> TransferToAsync(this Stream source, Stream destination, long maxLength, CancellationToken cancellationToken = default(CancellationToken))
+        {
+            using (var limitedStream = new SharpCompress.IO.ReadOnlySubStream(source, maxLength))
+            {
+                await limitedStream.CopyToAsync(destination, TEMP_BUFFER_SIZE, cancellationToken).ConfigureAwait(false);
+                return limitedStream.Position;
+            }
+        }
+
+        public static async Task SkipAsync(this Stream source, long advanceAmount, CancellationToken cancellationToken = default(CancellationToken))
         {
             if (source.CanSeek)
             {
@@ -167,56 +139,22 @@ internal static partial class Utility
                 return;
             }
 
-            var array = ArrayPool<byte>.Shared.Rent(TEMP_BUFFER_SIZE);
-            try
+
+            var buffer = new byte[TEMP_BUFFER_SIZE]; 
+            
+            while (advanceAmount > 0)
             {
-                while (advanceAmount > 0)
+                var toRead = (int)Math.Min(buffer.Length, advanceAmount);
+                var read = await source.ReadAsync(buffer, 0, toRead, cancellationToken).ConfigureAwait(false);
+                if (read <= 0)
                 {
-                    var toRead = (int)Math.Min(array.Length, advanceAmount);
-                    var read = await source
-                        .ReadAsync(array, 0, toRead, cancellationToken)
-                        .ConfigureAwait(false);
-                    if (read <= 0)
-                    {
-                        break;
-                    }
-                    advanceAmount -= read;
+                    break;
                 }
-            }
-            finally
-            {
-                ArrayPool<byte>.Shared.Return(array);
+                advanceAmount -= read;
             }
         }
 
-#if NET8_0_OR_GREATER
-        public bool ReadFully(byte[] buffer)
-        {
-            try
-            {
-                source.ReadExactly(buffer);
-                return true;
-            }
-            catch (EndOfStreamException)
-            {
-                return false;
-            }
-        }
-
-        public bool ReadFully(Span<byte> buffer)
-        {
-            try
-            {
-                source.ReadExactly(buffer);
-                return true;
-            }
-            catch (EndOfStreamException)
-            {
-                return false;
-            }
-        }
-#else
-        public bool ReadFully(byte[] buffer)
+        public static bool ReadFully(this Stream source, byte[] buffer)
         {
             var total = 0;
             int read;
@@ -231,26 +169,7 @@ internal static partial class Utility
             return (total >= buffer.Length);
         }
 
-        public bool ReadFully(Span<byte> buffer)
-        {
-            var total = 0;
-            int read;
-            while ((read = source.Read(buffer.Slice(total, buffer.Length - total))) > 0)
-            {
-                total += read;
-                if (total >= buffer.Length)
-                {
-                    return true;
-                }
-            }
-            return (total >= buffer.Length);
-        }
-#endif
-
-        public async ValueTask<bool> ReadFullyAsync(
-            byte[] buffer,
-            CancellationToken cancellationToken = default
-        )
+        public static async Task<bool> ReadFullyAsync(this Stream source, byte[] buffer, CancellationToken cancellationToken = default(CancellationToken))
         {
             var total = 0;
             int read;
@@ -271,12 +190,7 @@ internal static partial class Utility
             return (total >= buffer.Length);
         }
 
-        public async ValueTask<bool> ReadFullyAsync(
-            byte[] buffer,
-            int offset,
-            int count,
-            CancellationToken cancellationToken = default
-        )
+        public static async Task<bool> ReadFullyAsync(this Stream source, byte[] buffer, int offset, int count, CancellationToken cancellationToken = default(CancellationToken))
         {
             var total = 0;
             int read;
@@ -296,98 +210,60 @@ internal static partial class Utility
             }
             return (total >= count);
         }
-    }
 
-    /// <summary>
-    /// Read exactly the requested number of bytes from a stream. Throws EndOfStreamException if not enough data is available.
-    /// </summary>
-    public static void ReadExact(this Stream stream, byte[] buffer, int offset, int length)
-    {
-        if (stream is null)
+        public static void ReadExact(this Stream stream, byte[] buffer, int offset, int length)
         {
-            throw new ArgumentNullException(nameof(stream));
-        }
+            if (stream == null) throw new ArgumentNullException(nameof(stream));
+            if (buffer == null) throw new ArgumentNullException(nameof(buffer));
+            if (offset < 0 || offset > buffer.Length) throw new ArgumentOutOfRangeException(nameof(offset));
+            if (length < 0 || length > buffer.Length - offset) throw new ArgumentOutOfRangeException(nameof(length));
 
-        if (buffer is null)
-        {
-            throw new ArgumentNullException(nameof(buffer));
-        }
-
-        if (offset < 0 || offset > buffer.Length)
-        {
-            throw new ArgumentOutOfRangeException(nameof(offset));
-        }
-
-        if (length < 0 || length > buffer.Length - offset)
-        {
-            throw new ArgumentOutOfRangeException(nameof(length));
-        }
-
-        while (length > 0)
-        {
-            var fetched = stream.Read(buffer, offset, length);
-            if (fetched <= 0)
+            while (length > 0)
             {
-                throw new EndOfStreamException();
+                var fetched = stream.Read(buffer, offset, length);
+                if (fetched <= 0)
+                {
+                    throw new EndOfStreamException();
+                }
+
+                offset += fetched;
+                length -= fetched;
             }
-
-            offset += fetched;
-            length -= fetched;
         }
-    }
 
-    // Async methods moved to Utility.Async.cs
+        public static string TrimNulls(this string source) => source.Replace('\0', ' ').Trim();
 
-    public static string TrimNulls(this string source) => source.Replace('\0', ' ').Trim();
+        public static uint SwapUINT32(uint number) =>
+            (number >> 24)
+            | ((number << 8) & 0x00FF0000)
+            | ((number >> 8) & 0x0000FF00)
+            | (number << 24);
 
-    /// <summary>
-    /// Swap the endianness of a UINT32
-    /// </summary>
-    /// <param name="number">The UINT32 you want to swap his endianness</param>
-    /// <returns>Return the new UINT32 in the other endianness format</returns>
-    public static uint SwapUINT32(uint number) =>
-        (number >> 24)
-        | ((number << 8) & 0x00FF0000)
-        | ((number >> 8) & 0x0000FF00)
-        | (number << 24);
-
-    /// <summary>
-    /// Insert a little endian UINT32 into a byte array
-    /// </summary>
-    /// <param name="buffer">The buffer to insert into</param>
-    /// <param name="number">The UINT32 to insert</param>
-    /// <param name="offset">Offset of the buffer to insert into</param>
-    public static void SetLittleUInt32(ref byte[] buffer, uint number, long offset)
-    {
-        buffer[offset] = (byte)(number);
-        buffer[offset + 1] = (byte)(number >> 8);
-        buffer[offset + 2] = (byte)(number >> 16);
-        buffer[offset + 3] = (byte)(number >> 24);
-    }
-
-    /// <summary>
-    /// Insert a big endian UINT32 into a byte array
-    /// </summary>
-    /// <param name="buffer">The buffer to insert into</param>
-    /// <param name="number">The UINT32 to insert</param>
-    /// <param name="offset">Offset of the buffer to insert into</param>
-    public static void SetBigUInt32(ref byte[] buffer, uint number, long offset)
-    {
-        buffer[offset] = (byte)(number >> 24);
-        buffer[offset + 1] = (byte)(number >> 16);
-        buffer[offset + 2] = (byte)(number >> 8);
-        buffer[offset + 3] = (byte)number;
-    }
-
-    public static string ReplaceInvalidFileNameChars(string fileName)
-    {
-        var sb = new StringBuilder(fileName.Length);
-        foreach (var c in fileName)
+        public static void SetLittleUInt32(ref byte[] buffer, uint number, long offset)
         {
-            var newChar = invalidChars.Contains(c) ? '_' : c;
-            sb.Append(newChar);
+            buffer[offset] = (byte)(number);
+            buffer[offset + 1] = (byte)(number >> 8);
+            buffer[offset + 2] = (byte)(number >> 16);
+            buffer[offset + 3] = (byte)(number >> 24);
         }
 
-        return sb.ToString();
+        public static void SetBigUInt32(ref byte[] buffer, uint number, long offset)
+        {
+            buffer[offset] = (byte)(number >> 24);
+            buffer[offset + 1] = (byte)(number >> 16);
+            buffer[offset + 2] = (byte)(number >> 8);
+            buffer[offset + 3] = (byte)number;
+        }
+
+        public static string ReplaceInvalidFileNameChars(string fileName)
+        {
+            var sb = new StringBuilder(fileName.Length);
+            foreach (var c in fileName)
+            {
+                var newChar = invalidChars.Contains(c) ? '_' : c;
+                sb.Append(newChar);
+            }
+            return sb.ToString();
+        }
     }
 }
